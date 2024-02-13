0f21adb19824b51404d4a7d435b5cf3d234f0545
Aakash Sinha <sinha@Aakashs-MacBook-Air.local>
2024-02-13 21:10:32 +0530

    service-registory added

---
+++ service-registory/src/test/java/in/aakashsinha/serviceregistory/ServiceRegistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package in.aakashsinha.serviceregistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
